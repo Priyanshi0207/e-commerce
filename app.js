2c5b38743b7f66d1be456986e15cd4432c912473
Priyanshi dhakad <prayanshudhakad405@gmail.com>
2024-12-25 10:30:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//import logo from "./logo.svg";
+import "./app.css";
+
+function app(){
+    return(
+        <div className ="app">
+        <h1>E- Dasboard</h1>
+        </div>
+    );
+}
+
+export default app;
